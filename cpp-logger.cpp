7d7f6a578f542b99478d975a34ec06b809618511
Nathaniel Gunawan <nielchvo@gmail.com>
2020-02-28 03:30:17 +0700

    add parameter flow

--- cpp-logger.cpp
+++ cpp-logger.cpp
@@ -4,30 +4,18 @@
 #include <iostream>
 #include "Logger.h"
 
+
 int main(int argc, const char* argv[])
 {
-    std::cout << "Hello World!\n";
-
-	for (size_t i = 0; i < argc; i++)
+	if (argc != 5)
 	{
-		printf("%d - %s\n", i, argv[i]);
+		std::cout << "args must be contain -t <minute> and -d <input file>";
+		return 1;
 	}
 
-	
-	Logger::GetInstance()->readFile(argv[1], argv[4]);
+	Logger::GetInstance()->readFile(argv[2], argv[4]);
 	
 	Logger::GetInstance()->printStack();
 
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
